26f9253c5e61db5c1ffeed5161c35b022a85e8dd
buymetehbing <viivienneching@gmail.com>
2024-01-18 15:37:25 +0800

    problem 1 solution

---
+++ src/problem1/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Task 1
+ * 
+ * Provide 3 unique implementations of the following function in JavaScript.
+ * Input: `n` - any integer
+ * Assuming this input will always produce a result lesser than `Number.MAX_SAFE_INTEGER`
+ * Output: `return` - summation to `n`, i.e. `sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15`.
+ */
+
+// First Approach: Using mathematical formula
+var sum_to_n_a = function(n) {
+    return (n*(n+1)) / 2;
+};
+
+// Second Approach: Using for loop
+var sum_to_n_b = function(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+};
+
+// Third Approach: Using while loop
+var sum_to_n_c = function(n) {
+    let sum = 0, i = 1;
+    while(i <= n) {
+        sum += i;
+        i++;
+    }
+    return sum;
+};
